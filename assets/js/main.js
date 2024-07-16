922a5848aea7fe2789828e24ef40aea0667bad7f
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-07-16 09:52:37 -0300

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+function sum(val1, val2) {
+    return val1 + val2
+} 
+
+function subtraction(val1, val2) {
+    return val1 - val2
+} 
+
+function multiplication(val1, val2) {
+    return val1 * val2
+} 
+
+function division(val1, val2) {
+    return val1 / val2
+} 
